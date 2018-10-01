9e40db21a8d44f028c033318d4770f173ed586cf
Laurence Nairne <laurencenairne@gmail.com>
2018-10-01 22:28:37 +0100

    Updated to allow using passed cli variables as node arguments.

--- firstbashscript.sh
+++ firstbashscript.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -n "Hi there, type a message to display and press [Enter]: "
-read message
-MSG=message
-echo -n "Now please type today's date and press [Enter]: "
-read date
-DATE=date
-
-cd /d/Github_Repos/cli_json
-echo $MSG $DATE|node index.js
+node index "$1" "$2"
